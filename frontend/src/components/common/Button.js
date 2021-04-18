66900dbfbb4639305f8d24e5fdd0541e9ea874a4
이승윤 <syoon0624@khu.ac.kr>
2021-04-18 18:30:16 +0900

    feat: Common component 추가- Button

--- frontend/src/components/common/Button.js
+++ frontend/src/components/common/Button.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+import { buttonColorMap } from '../../lib/styles/palette';
 
-const Button = () => {
-  return <div>Button</div>;
+const ButtonBlock = styled.button`
+  width: 20%;
+  background-color: ${props => buttonColorMap[props.color].background};
+  &:hover {
+    background-color: ${props => buttonColorMap[props.color].hoverBackground};
+  }
+  color: ${props => buttonColorMap[props.color].color};
+  float: ${props => props.float || ''};
+`;
+
+const Button = ({ children, color, float }) => {
+  return (
+    <ButtonBlock color={color} float={float}>
+      {children}
+    </ButtonBlock>
+  );
 };
 
 export default Button;
