12317715bf200b9cc22f7668facbc436ff07a1c6
Hyeonguk Ahn <ahu8867@khu.ac.kr>
2021-04-26 20:34:15 +0900

    style: GlobalStyle 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  #root {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+  }
+  html {
+    box-sizing: border-box;
+
+    * {
+      box-sizing: inherit;
+    }
+  }
+`;
 
 const App = () => (
   <BrowserRouter>
+    <GlobalStyle />
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/login" exact component={LoginPage} />
     </Switch>
   </BrowserRouter>
 );
