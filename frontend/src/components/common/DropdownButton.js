6ccb8c3f3b08efbf1ff32a8eb578047a88937b82
이승윤 <syoon0624@khu.ac.kr>
2021-05-08 13:56:36 +0900

    style: Dropdown 폼 변경(ul-li -> select)

--- frontend/src/components/common/DropdownButton.js
+++ frontend/src/components/common/DropdownButton.js
@@ -6,14 +6,14 @@ const DropDownBlock = styled.div`
   margin: 0 auto;
   float: ${props => props.float || ''};
 `;
-
-const DropDownHeader = styled.button`
+const DropDownHeader = styled.select`
   margin-bottom: 0.8em;
   padding: 0.4em;
   width: ${props => props.width || '100px'};
-  line-height: ${props => props.height || '30px'};
+  height: ${props => props.height || '30px'};
   padding-right: 30px;
   font-size: ${props => props.fontsize || '20px'};
+  cursor: pointer;
   color: ${props => dropdownHeaderColorMap[props.color].color};
   background-color: ${props => dropdownHeaderColorMap[props.color].background};
   &:hover {
@@ -21,18 +21,37 @@ const DropDownHeader = styled.button`
       dropdownHeaderColorMap[props.color].hoverBackground};
   }
   border: 1px ${props => dropdownHeaderColorMap[props.color].background};
+
+  option {
+    color: blue;
+    background-color: blue;
+    display: flex;
+    white-space: pre;
+    min-height: 20px;
+    padding: 0px 2px 1px;
+  }
 `;
 
-const DropDown = ({ children, float, color, fontsize, width, height }) => {
+const DropDown = ({
+  options,
+  float,
+  color = 'blue',
+  fontsize,
+  width,
+  height,
+}) => {
   return (
     <DropDownBlock float={float} color={color}>
       <DropDownHeader
+        options={options}
         color={color}
         fontsize={fontsize}
         width={width}
         height={height}
       >
-        {children}
+        {options.map(friend => (
+          <option value={friend.id}>{friend.name}</option>
+        ))}
       </DropDownHeader>
     </DropDownBlock>
   );
