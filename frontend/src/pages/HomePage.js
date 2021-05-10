de64e2084d1aa2bd768a326d6f9cb7b550873cac
Hyeonguk Ahn <ahu8867@khu.ac.kr>
2021-05-10 17:10:32 +0900

    style: Homepage UI 컴포넌트 추가

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,15 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
+import Button from '../components/common/Button';
+import DropDownButton from '../components/common/DropdownButton';
+import Input from '../components/common/Input';
 
 const Main = styled.div`
-  width: 100%;
+  display: flex;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SearchBlock = styled.div`
+  display: flex;
+`;
+
+const LoginButtonBlock = styled.div`
+  position: fixed;
+  top: 20px;
+  right: 20px;
 `;
 
 const HomePage = () => {
   return (
-    <>
-      <Main>Home</Main>
-    </>
+    <Main>
+      <div>logo</div>
+      <SearchBlock>
+        <DropDownButton
+          fontsize="20px"
+          height="50px"
+          options={[
+            { id: 1, name: '전체' },
+            { id: 2, name: '개인' },
+            { id: 3, name: '부서' },
+          ]}
+        />
+        <Input color="blue" size="14px" width="400px" display />
+      </SearchBlock>
+
+      {/* Todo : 로그인 했을 경우 로그인 버튼 숨기기 */}
+      <LoginButtonBlock>
+        <Button>로그인</Button>
+      </LoginButtonBlock>
+    </Main>
   );
 };
 
