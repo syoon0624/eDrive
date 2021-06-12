74e36e62d609420980a9015cfbd12b3df6d74f21
이승윤 <syoon0624@khu.ac.kr>
2021-06-12 16:41:23 +0900

    feat: mock data 기준 templist 추가

--- frontend/src/components/document/Thumbnail.js
+++ frontend/src/components/document/Thumbnail.js
@@ -1,15 +1,20 @@
 import { Image, Text } from '@mantine/core';
 import React from 'react';
 
-const Thumbnails = () => {
+const Thumbnails = ({ srcs }) => {
   return (
     <>
       <Text style={{ marginTop: '1rem' }}>문서 내 이미지</Text>
       <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Image width={200} height={250} radius="md" withPlaceholder />
-        <Image width={200} height={250} radius="md" withPlaceholder />
-        <Image width={200} height={250} radius="md" withPlaceholder />
-        <Image width={200} height={250} radius="md" withPlaceholder />
+        {srcs.map(src => (
+          <Image
+            width={200}
+            height={250}
+            radius="md"
+            src={src}
+            withPlaceholder
+          />
+        ))}
       </div>
     </>
   );
